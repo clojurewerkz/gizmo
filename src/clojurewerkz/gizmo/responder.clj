9413c251808a4d109e99a603d23ca21100b3c5b7
Alex Petrov <alexp@coffeenco.de>
2013-09-02 23:20:10 +0200

    Add responder

---
+++ src/clojurewerkz/gizmo/responder.clj
@@ -0,0 +1,53 @@
+(ns clojurewerkz.gizmo.responder
+  (:require [clojurewerkz.gizmo.widget :as widget]
+            [clojurewerkz.gizmo.request :as request]
+            [clojurewerkz.gizmo.utils.hash-utils :as hash-utils]
+
+            [cheshire.core :as json]
+            [net.cgrand.enlive-html :as html]))
+
+(defn render
+  [nodes]
+  (apply str (html/emit* nodes)))
+
+(defmulti respond-with (fn [{:keys [render] :or {:render :html}}]
+                         render))
+
+(defmethod respond-with :nothing
+  [env]
+  {:body ""})
+
+(defmethod respond-with :json
+  [env]
+  (let [response (json/generate-string (or (:response-hash env) {}))]
+    {:headers (merge (:headers env)
+                     {"Content-Type"  "application/json; charset=utf-8"
+                      "Content-Length" (str (count response))})
+     :status (or (:status env) 200)
+     :body response}))
+
+(defmethod respond-with :html
+  [{:keys [widgets status headers layout] :as env}]
+  (assert (> (count (widget/all-layouts)) 0) "Can't respond with :html without layouts given<")
+  (let [layout-template (if layout
+                          (get (widget/all-layouts) layout)
+                          (last (first (widget/all-layouts))))
+        response        (request/with-request env
+                          (render
+                           (widget/interpolate-widgets
+                            (widget/inject-core-widgets (layout-template)
+                                                        (:widgets env))
+                            env)))]
+    {:headers (merge headers
+                     {"Content-Type"  "text/html; charset=utf-8"
+                      "Content-Length" (str (count response))})
+     :status (or status 200)
+     :body response}))
+
+(defn wrap-responder
+  [handler]
+  (fn [env]
+    (let [handler-env  (handler env)
+          complete-env (hash-utils/deep-merge env handler-env)]
+      (println "Handling uri: " (:uri env) " Rendering: " (:render complete-env))
+      (respond-with complete-env))))
---
+++ test/clojurewerkz/gizmo/responder_test.clj
@@ -0,0 +1,59 @@
+(ns clojurewerkz.gizmo.responder-test
+  (:use clojure.test
+        clojurewerkz.gizmo.responder
+        clojurewerkz.gizmo.widget))
+
+(deftest respond-with-html
+  (deflayout respond-with-html-layout "templates/layouts/application.html" [])
+  (defwidget respond-with-html-header-widget :view (fn [_] "header"))
+  (defwidget respond-with-html-content-widget :view (fn [_] "content"))
+
+  (let [res (respond-with {:render  :html
+                           :widgets {:content 'clojurewerkz.gizmo.responder-test/respond-with-html-content-widget
+                                     :header 'clojurewerkz.gizmo.responder-test/respond-with-html-header-widget}})]
+    (is (= "173" (get-in res [:headers "Content-Length"])))
+    (is (= "text/html; charset=utf-8" (get-in res [:headers "Content-Type"])))
+    (is (= "<!DOCTYPE html>
+<html lang=\"en\">
+  <head>
+    <title>App Layout</title>
+  </head>
+  <body>
+    header
+    <div id=\"content-main\">
+      content
+    </div>
+  </body>
+
+</html>" (:body res)))
+    (is (= 200 (get-in res [:status])))))
+
+(deftest respond-with-json
+  (let [res (respond-with {:render        :json
+                           :response-hash {:response :hash}})]
+    (is (= "19" (get-in res [:headers "Content-Length"])))
+    (is (= "application/json; charset=utf-8" (get-in res [:headers "Content-Type"])))
+    (is (= "{\"response\":\"hash\"}" (:body res)))
+    (is (= 200 (get-in res [:status])))))
+
+
+(deftest respond-with-custom-status
+  (let [res (respond-with {:render :json
+                           :status 404
+                           :response-hash {:response :hash}})]
+    (is (= 404 (:status res)))))
+
+(deftest wrap-responder-test
+  (let [env     (atom nil)
+        handler (wrap-responder
+                 (fn [e]
+                   (reset! env e)
+                   {:response-hash {:response :hash}
+                    :status 201
+                    :render :json}))
+        res     (handler {:env :env})]
+    (testing "Handler receives env from middleware"
+      (is (= {:env :env} @env)))
+    (testing "Response depends on outcome of handler"
+      (is (= 201 (:status res)))
+      (is (= "{\"response\":\"hash\"}" (:body res))))))
