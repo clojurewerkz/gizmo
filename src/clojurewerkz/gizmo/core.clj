0c35112c3b7259933308f8029a4b31f0edaff456
Alex Petrov <alexp@coffeenco.de>
2013-09-16 13:54:30 +0200

    Add docstrings

--- src/clojurewerkz/gizmo/core.clj
+++ src/clojurewerkz/gizmo/core.clj
@@ -1,4 +1,6 @@
 (ns clojurewerkz.gizmo.core
+  "Gizmo core, set of functions for the application to start, based on Gizmo conventions for
+   location of widgets, snippets, handlers and services."
   (:require [net.cgrand.reload :as enlive-reload]
             [bultitude.core :as bultitude]))
 
@@ -8,22 +10,27 @@
     (require ns)))
 
 (defn require-widgets
+  "Loads all the namespaces containing widgets"
   [^String app]
   (require-all (str app ".widgets")))
 
 (defn require-snippets
+  "Loads all the namespaces containing snippets"
   [^String app]
   (require-all (str app ".snippets")))
 
 (defn require-handlers
+  "Loads all the namespaces containing handlers"
   [^String app]
   (require-all (str app ".handlers")))
 
 (defn require-services
+  "Loads all the namespaces containing services"
   [^String app]
   (require-all (str app ".services")))
 
 (defn register-snippet-reload
+  "Registers snippet reloading in Enlive"
   [name]
   (doseq [ns (bultitude/namespaces-on-classpath :prefix (str name ".snippets"))]
     (enlive-reload/auto-reload (find-ns ns))))
