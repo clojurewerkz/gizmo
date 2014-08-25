0dce4767f1d5053a814efdd818a44ca3580f7011
Alex Petrov <alexp@coffeenco.de>
2014-08-25 10:46:10 +0200

    Fix typo

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [enlive "1.1.5"]
                  [cheshire "5.3.1"]
-                 [clojurewerkz/route-one "1.1.0-SNAPSHOT"]
+                 [clojurewerkz/route-one "1.1.0"]
                  [com.taoensso/timbre "3.0.1"]
 
                  [ring/ring "1.2.1"]
